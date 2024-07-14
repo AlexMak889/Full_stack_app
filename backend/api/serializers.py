0bc9c4a6a86f908b040af16c45e6beb163a73b0a
Alex <alexmak889@gmail.com>
2024-07-14 13:38:28 +0200

    first commit

---
+++ backend/api/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Note
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+    
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = None
+        fields = ["id", "title", "content", "created_att", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
